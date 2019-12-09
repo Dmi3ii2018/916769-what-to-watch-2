99628d1b77f16b78e8cd839fd6d9e2d459b7a5a4
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-12-09 19:48:13 +0500

    создал API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
-import {filterReducer} from "./reducer/reducer";
+import {createStore, applyMiddleware} from "redux";
+import {filterReducer, Operation} from "./reducer/reducer";
 import {MainPage} from "./components/main/main";
-// import {films} from "./moks/film";
-
-const store = createStore(filterReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
-);
+import thunk from "redux-thunk";
+import {compose} from "recompose";
+import {createAPI} from './axios/api';
 
 const init = () => {
 
+  const api = createAPI((...args) => store.dispatch(...args));
+
+  const store = createStore(
+      filterReducer,
+      compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      )
+  );
+
+  store.dispatch(Operation.loadQuestions());
+
   ReactDOM.render(
       <Provider store={store}>
-        <MainPage onHeaderClic={() => {}}/>
+        <MainPage onHeaderClick={() => {}}/>
       </Provider>,
       document.querySelector(`#root`)
   );
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -10,12 +10,49 @@ const initialState = {
   filmsList: films,
 };
 
+const ActionType = {
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+  LOAD_FILMS: `LOAD_FILMS`,
+};
+
+
+export const ActionCreator = {
+  requireAuthorization: (status) => {
+    return {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: status,
+    };
+  },
+
+  loadFilms: (film) => {
+    return {
+      type: ActionType.LOAD_FILMS,
+      payload: film,
+    };
+  },
+};
+
+export const Operation = {
+  loadQuestions: () => (dispatch, _getState, api) => {
+    return api.get(`/films`)
+      .then((response) => {
+        dispatch(ActionCreator.loadFilms(response.data));
+      });
+  },
+};
+
 export const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case `SET_FILTER`:
       return Object.assign({}, state, {
         genre: action.genre,
       });
+
+    case `LOAD_FILMS`:
+      return Object.assign({}, state, {
+        filmsList: action.films
+      });
+
     default:
       return state;
   }
